5f7095cda336688f7fdbd267346ed08e7e405eda
Mary <mail@marygrooteman.com>
2018-01-30 10:38:13 +0100

    Update dutch.php
    Updated, used  less foral text. what about the words with 's, wil thate give any problems?
    Not sure If this is the right one, is says version 4, changed it to 5.2, is that right?

--- dutch.php
+++ dutch.php
@@ -5,18 +5,18 @@ $lang_cancel = "Annuleren";
 $lang_help = "Help";
 $lang_save = "Opslaan";
 $lang_rename_btn = "Hernoemen";
-$lang_version = "Pulse CMS 4.5.2";
+$lang_version = "Pulse CMS 5.2";
 $lang_update = "Update";
 
 // Login
 
-$lang_login_incorrect = "Uw wachtwoord is incorrect!";
+$lang_login_incorrect = "Verkeerd wachtwoord...";
 $lang_login_password = "Wachtwoord";
 $lang_login_button = "Inloggen";
 $lang_nav_logout = "Uitloggen";
 
 // Home
-$lang_home_emptyfold = "Deze folder is leeg."; 
+$lang_home_emptyfold = "Deze map is leeg."; 
 $lang_home_new = "Nieuw";
 $lang_home_upload_button = "Uploaden";
 $lang_home_preview = "Voorvertoning";
@@ -24,57 +24,57 @@ $lang_home_preview = "Voorvertoning";
 // Navigation
 
 $lang_nav_home = "Home";
-$lang_nav_blocks = "Elementen";
+$lang_nav_blocks = "Blocks";
 $lang_nav_img = "Media";
 $lang_nav_blog = "Blog";
 $lang_nav_pages = "Pagina's";
 
 // Gallery
 
-$lang_gal_caption_gallery = "Voeg bijschrift in";
+$lang_gal_caption_gallery = "Voeg omschrijving in. Deze tekst verschijnt onder de uitvergrote afbeelding.";
 $lang_gal_filename = "Bestandsnaam";
 $lang_gal_dimensions = "Afmetingen";
 $lang_gal_size = "Grootte";
 $lang_gal_img = "IMG";
 $lang_gal_link = "Link";
-$lang_gallery_tool_tip = "To create a lightbox gallery, create a folder of images and use the embed tag.";
+$lang_gallery_tool_tip = "Om een lightbox gallery te maken maak je eerst een map met afbeeldingen en dan gebruik je de invoeg-tag.";
 
 // Pages
 
-$lang_pages_title = "Paginatitel";
-$lang_pages_description = "Paginabeschrijving";
+$lang_pages_title = "Pagina-titel";
+$lang_pages_description = "Pagina-omschrijving";
 
 // Create
 
 $lang_create_file_or_folder = 'Nieuw bestand of folder';
 $lang_create_button = "Aanmaken"; 
-$lang_create_tool_tip = "Nieuwe pagina's, elementen of blog-posts moeten eindigen op '.txt'. Nieuwe folders krijgen géén bestands-extentie.";
+$lang_create_tool_tip = "Nieuwe pagina's, blocks of blog-posts moeten eindigen op '.txt'. Nieuwe mappen krijgen géén bestands-extentie.";
 
 // Delete
 
-$lang_delete = 'Weet u zeker dat u dit bestand wilt verwijderen: ';
+$lang_delete = 'Weet je zeker dat je dit bestand wilt verwijderen: ';
 $lang_del_button = "Verwijderen";
 
 // Errors
 
-$lang_error_create_ext = "U kunt alleen '.txt'-bestanden en folders aanmaken.";
-$lang_error_file_exists = 'In deze folder bestaat er reeds een bestand met deze naam.';
-$lang_error_upload = 'U kunt alleen afbeeldingen uploaden vanuit een Gallerij.';
+$lang_error_create_ext = "Je kunt alleen '.txt' bestanden en mappen aanmaken.";
+$lang_error_file_exists = 'In deze map bestaat al een bestand met deze naam.';
+$lang_error_upload = 'Je kunt alleen afbeeldingen uploaden vanuit een Gallery.';
 $lang_no_content = 'Geen inhoud gevonden';
 
 //Blog
 
 $lang_blog_read_more = 'Lees verder';
-$lang_blog_older = 'Ouder';
-$lang_blog_newer = 'Nieuwer';
-$lang_blog_error_folder = "U kunt binnen 'Blog' geen folder aanmaken.";
-$lang_blog_tool_tip = "Blog entries are ordered by number, the highest numbering being the newest post.";
+$lang_blog_older = 'Eerdere';
+$lang_blog_newer = 'Nieuwere';
+$lang_blog_error_folder = "Je kunt binnen de 'Blog' geen map aanmaken.";
+$lang_blog_tool_tip = "Blog posts zijn genummerd in volgorde; het hoogste nummer is de laatste post.";
 
 
 //Form
 
-$lang_form_error1 = 'Vult u alstublieft dit veld in ';
-$lang_form_error2a = 'Uw veld ';
+$lang_form_error1 = 'Vul alstublieft dit veld in ';
+$lang_form_error2a = 'Het veld ';
 $lang_form_error2b = ' is te lang.';
 $lang_form_subject_line = 'Contact';
 $lang_form_email_sent = 'Email is verzonden';
@@ -82,15 +82,15 @@ $lang_form_sent_button = 'Verzenden';
 
 // Stats
 
-$lang_stats_today = "Visitors";
-$lang_stats_refers = "Top Referrers";
-$lang_stats_pages = "Top Pages";
-$lang_stats_refresh = "Refresh";
+$lang_stats_today = "Bezoekers";
+$lang_stats_refers = "Top Verwijzers";
+$lang_stats_pages = "Top Pagina's";
+$lang_stats_refresh = "Ververs";
 $lang_stats_pageviews = "Pageviews";
-$lang_stats_per_visit = "Pages / Visit";
+$lang_stats_per_visit = "Pagina's / Bezoek";
 $lang_stats_online = "Online";
-$lang_stats_thisweek = "This Week";
-$lang_stats_todays_stats = "Today";
-$lang_stats_nodata = "No Data";
+$lang_stats_thisweek = "Deze Week";
+$lang_stats_todays_stats = "Vandaag";
+$lang_stats_nodata = "Geen Data";
 
 ?>
